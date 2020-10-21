4f080548dd6ec9a47a6873bd7493eec19db8246d
Ben <bchang@neosem.com>
2020-10-21 13:38:10 +0900

    add some web applications

---
+++ uncleBen/www/todo/application/logs/log-2020-09-06.php
@@ -0,0 +1,90 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+DEBUG - 2020-09-06 00:06:23 --> Config Class Initialized
+DEBUG - 2020-09-06 00:06:23 --> Hooks Class Initialized
+DEBUG - 2020-09-06 00:06:23 --> Utf8 Class Initialized
+DEBUG - 2020-09-06 00:06:23 --> UTF-8 Support Enabled
+DEBUG - 2020-09-06 00:06:23 --> URI Class Initialized
+DEBUG - 2020-09-06 00:06:23 --> Router Class Initialized
+DEBUG - 2020-09-06 00:06:23 --> Output Class Initialized
+DEBUG - 2020-09-06 00:06:23 --> Security Class Initialized
+DEBUG - 2020-09-06 00:06:23 --> Input Class Initialized
+DEBUG - 2020-09-06 00:06:23 --> Global POST and COOKIE data sanitized
+DEBUG - 2020-09-06 00:06:23 --> Language Class Initialized
+DEBUG - 2020-09-06 00:06:23 --> Loader Class Initialized
+DEBUG - 2020-09-06 00:06:23 --> Helper loaded: date_helper
+DEBUG - 2020-09-06 00:06:23 --> Controller Class Initialized
+DEBUG - 2020-09-06 00:06:23 --> Database Driver Class Initialized
+ERROR - 2020-09-06 00:06:23 --> Severity: 8192  --> mysql_connect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /uncleben/www/todo/system/database/drivers/mysql/mysql_driver.php 73
+DEBUG - 2020-09-06 00:06:23 --> Model Class Initialized
+DEBUG - 2020-09-06 00:06:23 --> Model Class Initialized
+DEBUG - 2020-09-06 00:06:23 --> Helper loaded: url_helper
+DEBUG - 2020-09-06 00:06:23 --> File loaded: application/views/todo/sentences_v.php
+DEBUG - 2020-09-06 00:06:23 --> Final output sent to browser
+DEBUG - 2020-09-06 00:06:23 --> Total execution time: 0.1443
+DEBUG - 2020-09-06 07:08:45 --> Config Class Initialized
+DEBUG - 2020-09-06 07:08:45 --> Hooks Class Initialized
+DEBUG - 2020-09-06 07:08:45 --> Utf8 Class Initialized
+DEBUG - 2020-09-06 07:08:45 --> UTF-8 Support Enabled
+DEBUG - 2020-09-06 07:08:45 --> URI Class Initialized
+DEBUG - 2020-09-06 07:08:45 --> Router Class Initialized
+DEBUG - 2020-09-06 07:08:45 --> Output Class Initialized
+DEBUG - 2020-09-06 07:08:45 --> Security Class Initialized
+DEBUG - 2020-09-06 07:08:45 --> Input Class Initialized
+DEBUG - 2020-09-06 07:08:45 --> Global POST and COOKIE data sanitized
+DEBUG - 2020-09-06 07:08:45 --> Language Class Initialized
+DEBUG - 2020-09-06 07:08:45 --> Loader Class Initialized
+DEBUG - 2020-09-06 07:08:45 --> Helper loaded: date_helper
+DEBUG - 2020-09-06 07:08:45 --> Controller Class Initialized
+DEBUG - 2020-09-06 07:08:45 --> Database Driver Class Initialized
+ERROR - 2020-09-06 07:08:45 --> Severity: 8192  --> mysql_connect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /uncleben/www/todo/system/database/drivers/mysql/mysql_driver.php 73
+DEBUG - 2020-09-06 07:08:45 --> Model Class Initialized
+DEBUG - 2020-09-06 07:08:45 --> Model Class Initialized
+DEBUG - 2020-09-06 07:08:45 --> Helper loaded: url_helper
+DEBUG - 2020-09-06 07:08:45 --> File loaded: application/views/todo/sentences_v.php
+DEBUG - 2020-09-06 07:08:45 --> Final output sent to browser
+DEBUG - 2020-09-06 07:08:45 --> Total execution time: 0.1879
+DEBUG - 2020-09-06 11:29:31 --> Config Class Initialized
+DEBUG - 2020-09-06 11:29:31 --> Hooks Class Initialized
+DEBUG - 2020-09-06 11:29:31 --> Utf8 Class Initialized
+DEBUG - 2020-09-06 11:29:31 --> UTF-8 Support Enabled
+DEBUG - 2020-09-06 11:29:31 --> URI Class Initialized
+DEBUG - 2020-09-06 11:29:31 --> Router Class Initialized
+DEBUG - 2020-09-06 11:29:31 --> Output Class Initialized
+DEBUG - 2020-09-06 11:29:31 --> Security Class Initialized
+DEBUG - 2020-09-06 11:29:31 --> Input Class Initialized
+DEBUG - 2020-09-06 11:29:31 --> Global POST and COOKIE data sanitized
+DEBUG - 2020-09-06 11:29:31 --> Language Class Initialized
+DEBUG - 2020-09-06 11:29:31 --> Loader Class Initialized
+DEBUG - 2020-09-06 11:29:31 --> Helper loaded: date_helper
+DEBUG - 2020-09-06 11:29:31 --> Controller Class Initialized
+DEBUG - 2020-09-06 11:29:31 --> Database Driver Class Initialized
+ERROR - 2020-09-06 11:29:31 --> Severity: 8192  --> mysql_connect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /uncleben/www/todo/system/database/drivers/mysql/mysql_driver.php 73
+DEBUG - 2020-09-06 11:29:31 --> Model Class Initialized
+DEBUG - 2020-09-06 11:29:31 --> Model Class Initialized
+DEBUG - 2020-09-06 11:29:31 --> Helper loaded: url_helper
+DEBUG - 2020-09-06 11:29:31 --> File loaded: application/views/todo/sentences_v.php
+DEBUG - 2020-09-06 11:29:31 --> Final output sent to browser
+DEBUG - 2020-09-06 11:29:31 --> Total execution time: 0.1098
+DEBUG - 2020-09-06 13:43:30 --> Config Class Initialized
+DEBUG - 2020-09-06 13:43:30 --> Hooks Class Initialized
+DEBUG - 2020-09-06 13:43:30 --> Utf8 Class Initialized
+DEBUG - 2020-09-06 13:43:30 --> UTF-8 Support Enabled
+DEBUG - 2020-09-06 13:43:30 --> URI Class Initialized
+DEBUG - 2020-09-06 13:43:30 --> Router Class Initialized
+DEBUG - 2020-09-06 13:43:30 --> Output Class Initialized
+DEBUG - 2020-09-06 13:43:30 --> Security Class Initialized
+DEBUG - 2020-09-06 13:43:30 --> Input Class Initialized
+DEBUG - 2020-09-06 13:43:30 --> Global POST and COOKIE data sanitized
+DEBUG - 2020-09-06 13:43:30 --> Language Class Initialized
+DEBUG - 2020-09-06 13:43:30 --> Loader Class Initialized
+DEBUG - 2020-09-06 13:43:30 --> Helper loaded: date_helper
+DEBUG - 2020-09-06 13:43:30 --> Controller Class Initialized
+DEBUG - 2020-09-06 13:43:30 --> Database Driver Class Initialized
+ERROR - 2020-09-06 13:43:30 --> Severity: 8192  --> mysql_connect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /uncleben/www/todo/system/database/drivers/mysql/mysql_driver.php 73
+DEBUG - 2020-09-06 13:43:30 --> Model Class Initialized
+DEBUG - 2020-09-06 13:43:30 --> Model Class Initialized
+DEBUG - 2020-09-06 13:43:30 --> Helper loaded: url_helper
+DEBUG - 2020-09-06 13:43:30 --> File loaded: application/views/todo/sentences_v.php
+DEBUG - 2020-09-06 13:43:30 --> Final output sent to browser
+DEBUG - 2020-09-06 13:43:30 --> Total execution time: 0.0953
